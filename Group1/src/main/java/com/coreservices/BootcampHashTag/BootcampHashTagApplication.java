cc1c760fdd11184267d340e2fe56c30f401bada8
PrzemMotala <var.crafter@gmail.com>
2018-11-05 20:54:58 +0100

    Refactoring

--- Group1/src/main/java/com/coreservices/BootcampHashTag/BootcampHashTagApplication.java
+++ Group1/src/main/java/com/coreservices/BootcampHashTag/BootcampHashTagApplication.java
@@ -1,13 +1,13 @@
 package com.coreservices.BootcampHashTag;
 
 import com.coreservices.configuration.WebAppConfiguration;
-import com.coreservices.service.FileProcessorService;
+import com.coreservices.configuration.FileProcessorConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({WebAppConfiguration.class, FileProcessorService.class})
+@Import({WebAppConfiguration.class, FileProcessorConfiguration.class})
 public class BootcampHashTagApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootcampHashTagApplication.class, args);
